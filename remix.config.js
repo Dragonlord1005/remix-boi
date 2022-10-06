36fa4d35571ed13139327c6f2058aa96f7418a1b
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-06 19:00:40 +0000

    stuff

--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,5 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
 };
